ec3aff3b56546eb3de7a748e67aa0f5c5c059792
konjing <konjingtuajing@gmail.com>
2020-05-10 07:21:19 +0700

    Push staticfiles

---
+++ staticfiles/app_sme12/js/sme_info.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+      
+    // $(".progress-bar").change(function () {
+    //   var revenuecode = $("#id_f_revenue option:selected").data("code"); // get the selected  data-code from the HTML input
+  
+    //   if (revenuecode == "301") {
+    //     $("div.revenue_etc").show();
+    //     $("#id_f_revenue_etc").maskMoney({ precision: 0 });
+    //     return true;
+    //   } else {
+    //     $("#id_f_revenue_etc").val("");
+    //     $("div.revenue_etc").hide();
+    //     return true;
+    //   }
+    // });
+
+});
+  
